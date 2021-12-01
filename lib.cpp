0e7a8c013a11604987140315a11818db97195d89
Tommaso Scorza <91656025+scovza@users.noreply.github.com>
2021-12-01 15:15:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,29 @@
+bool det(int a) {
 
+    int c;
+    int count = 0;
+
+    c = 1;
+
+    if (a > 1) {
+
+        while (count <= 1 && c <= a / 2) {
+            if (a % c == 0) {
+
+                count++;
+            }
+            c++;
+        }
+
+        if (count == 1) {
+
+            return true;
+        }else {
+
+            return false;
+        }
+    }else {
+
+        return false;
+    }
+}
